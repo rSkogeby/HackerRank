ba22e24cbd817d25d6c34d45b9c1e8f512c3138c
Richard Skogeby <richard.skogeby@outlook.com>
2018-02-13 16:57:36 +0100

    Solution to HackerRank problem: C++ Variadics

---
+++ HackerRank_cpp_Variadics.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <array>
+#include <bitset>
+using namespace std;
+
+// Enter your code for reversed_binary_value<bool...>()
+template <bool ...digits>
+int reversed_binary_value(){
+    std::array<bool, sizeof...(digits)> ar{digits...};
+    std::bitset<sizeof...(digits)> bs; 
+    for(std::size_t i = 0; i < ar.size(); ++i) {
+        bs[i] = ar[i];
+    }
+    return bs.to_ulong();
+}
+
+// Added by HackerRank:
+template <int n, bool...digits>
+struct CheckValues {
+  	static void check(int x, int y)
+  	{
+    	CheckValues<n-1, 0, digits...>::check(x, y);
+    	CheckValues<n-1, 1, digits...>::check(x, y);
+  	}
+};
+
+template <bool...digits>
+struct CheckValues<0, digits...> {
+  	static void check(int x, int y)
+  	{
+    	int z = reversed_binary_value<digits...>();
+    	std::cout << (z+64*y==x);
+  	}
+};
+
+int main()
+{
+  	int t; std::cin >> t;
+
+  	for (int i=0; i!=t; ++i) {
+		int x, y;
+    	cin >> x >> y;
+    	CheckValues<6>::check(x, y);
+    	cout << "\n";
+  	}
+}
--- README.md
+++ README.md
@@ -9,3 +9,5 @@ Attending Workshops | HackerRank\_Workshops.cpp
 Bit Array | HackerRank\_Brents\_Algorithm\_Cyclic\_Detection.cpp
 
 Quadrant Queries | HackerRank\_Quadrant\_Queries.cpp
+
+C++ Variadics | HackerRank\_cpp\_Variadics.cpp
